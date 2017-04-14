7d1420daba99f701603cd4fcce6229e5b5b04a88
x22a <hex22a@gmail.com>
2017-04-14 03:54:34 +0300

    draft

--- src/components/common/inputs/Input/Input.js
+++ src/components/common/inputs/Input/Input.js
@@ -1,18 +1,23 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import s from './input.pcss';
 
-const Input = ({ input, placeholder, type }) =>
+const Input = ({ input, placeholder, type, meta: { touched, error } = {}}) =>
   <div>
     <input
       type={ type }
       { ...input }
       placeholder={ placeholder }
-      className={ s.filterInput }
+      className={ touched && error ? s['input-error'] : s.input }
     />
+    {touched && error &&
+      <div className={ s.error }>{ error }</div>
+    }
   </div>;
 
 Input.propTypes = {
   input: PropTypes.object,
+  meta: PropTypes.object,
   placeholder: PropTypes.string,
   type: PropTypes.string,
 };
