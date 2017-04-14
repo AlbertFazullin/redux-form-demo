78b7604d0518d55e9fc13d1710e3c033e6191f74
Albert Fazullin <albert.fazullin@flatstack.com>
2017-04-14 14:59:24 +0300

    4step

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 
 import Input from '../common/inputs/Input/Input';
+import CheckBox from '../common/inputs/CheckBox/CheckBox';
+import Radios from '../common/inputs/Radios/Radios';
 import s from './filter.pcss';
 
 import submit from './submit';
@@ -18,7 +20,50 @@ const Filter = ({ reset, handleSubmit }) =>
           component={ Input }
           type="text"
           placeholder="Placeholder"
-          name="text input"
+          name="text_input"
+        />
+      </div>
+      <div className={ s.filterItem }>
+        <h4 className={ s.filterTitle }>
+          Checbox group 1
+        </h4>
+        <Field
+          component={ CheckBox }
+          fields={ [
+            { name: 'foo', label: 'foo' },
+            { name: 'bar', label: 'bar' },
+          ] }
+          name="cb_group1"
+        />
+      </div>
+      <div className={ s.filterItem }>
+        <h4 className={ s.filterTitle }>
+          CheckBox group 2
+        </h4>
+        <Field
+          component={ CheckBox }
+          fields={ [
+            { name: 'foo1', label: 'foo1' },
+            { name: 'bar2', label: 'bar2' },
+            { name: 'chai', label: 'chai' },
+            { name: 'pivo', label: 'pivo' },
+            { name: 'vape', label: 'vape' },
+          ] }
+          name="cb_group2"
+        />
+      </div>
+      <div className={ s.filterItem }>
+        <h4 className={ s.filterTitle }>
+          Radio
+        </h4>
+        <Field
+          component={ Radios }
+          fields={ [
+            { name: 'oldest', label: 'Oldest' },
+            { name: 'newest', label: 'Newest' },
+            { name: 'eta', label: 'ETA' },
+          ] }
+          name="sort_by"
         />
       </div>
       <div className={ s.filterBottom }>
@@ -42,5 +87,9 @@ Filter.propTypes = {
 };
 
 export default reduxForm({
-  form: 'filter-1',
+  form: 'filter',
+  initialValues: {
+    sort_by: 'newest',
+    cb_group2: ['pivo'],
+  },
 })(Filter);
