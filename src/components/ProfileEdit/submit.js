41d13808c56ecc3272964e09c99097b734d26ceb
x22a <hex22a@gmail.com>
2017-04-14 10:16:32 +0300

    boring subit validation

--- src/components/ProfileEdit/submit.js
+++ src/components/ProfileEdit/submit.js
@@ -1,5 +1,12 @@
-const submit = values => {
-  alert(JSON.stringify(values));
+import { SubmissionError } from 'redux-form';
+
+const submit = ({ username, password }) => {
+  if (!password || password.length < 6) {
+    throw new SubmissionError({ password: 'Password must be at least 6 characters', _error: 'Submit failed' });
+  }
+  if (!username) {
+    throw new SubmissionError({ username: 'Username can not be empty', _error: 'Submit failed' });
+  }
 };
 
 export default submit;
